16e451d46f115f2403a78618df5005e7daf6dc96
yusufkt0 <yusufkayrataner3@gmail.com>
2024-02-19 22:50:55 +0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <opencv4/opencv2/videoio.hpp>
+#include <ostream>
+#include "opencv2/opencv.hpp"
+#include "opencv2/core/version.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/opencv.hpp"
+#include "opencv2/videoio.hpp"
+
+using namespace cv;
+int main(){
+    std::cout << CV_VERSION;
+    
+    VideoCapture cap(0, CAP_V4L2);
+    if(!cap.isOpened()){
+        std::cout << "nah";
+
+    }
+    namedWindow("window",WINDOW_NORMAL);
+
+    while (true) {
+        Mat frame;
+        cap >> frame;
+        if (frame.empty()) {
+            std::cerr << "nggr" << std::endl;
+            break;
+        }
+        Mat gray;
+        cvtColor(frame, gray, COLOR_BGR2GRAY);
+        Mat threshed;        
+        adaptiveThreshold(gray, threshed, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY, 11, 2);    
+        imshow("n",frame);
+        imshow("t",threshed);
+    }
+}
