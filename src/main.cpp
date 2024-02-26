7c6a15b15f3ead073413afb4a583adaf3d5467a9
yusufkt0 <yusufkayrataner3@gmail.com>
2024-02-26 21:59:01 +0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include "main.hpp"
 #include <iostream>
+#include <opencv2/core/mat.hpp>
 #include <opencv2/videoio.hpp>
 
 
 int main(){
     cv::VideoCapture cap(0,cv::CAP_V4L2);
+    
     if(!cap.isOpened()){
         std::cerr << "cam not opened";
     }
+    cv::Mat frame;
+    while (true) {
+        cap >> frame;
+        if (frame.empty()) {
+            std::cerr << "frame empty";
+        
+        }
+    
+    }
+
 
 }
